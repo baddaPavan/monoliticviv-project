04ccb1d3054db6d3c84aeb4abe820cc02c86aa0d
vivekvicky545 <131896557+vivekvicky545@users.noreply.github.com>
2023-06-29 20:04:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ key_name = "vivekslavekey"
 count = 1
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
-Name = "project-instance"
+Name = "project-instance-1"
 Environment = "dev"
 }
 }
