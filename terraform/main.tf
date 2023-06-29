134c06f117e18ad2f127d033612e1164242e36e0
root <root@ip-172-31-82-158.ec2.internal>
2023-06-29 09:30:45 +0000

    hi this is terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+region = "us-east-1"
+access_key ="AKIAVAMDXMJYKHSHFQX7"
+secret_key ="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"
+}
+resource "aws_instance" "one" {
+
+ami = "ami-090e0fc566929d98b"
+instance_type = "t2.micro"
+key_name = "vivekslavekey"
+vpc_security_group_ids = [aws_security_group.sec.id]
+tags = {
+Name = "vivekprod"
+Environment = "prod"
+}
